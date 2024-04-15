bb3baf7441933b9afca91a5c91c241f241d48aa1
Kevinetiogouo <167082590+Kevinetiogouo@users.noreply.github.com>
2024-04-16 00:50:01 +0100

    Update main.R

--- main.R
+++ main.R
@@ -2,7 +2,10 @@ library(tidyverse)
 library(here)
 
 tv_hours_table <- gss_cat %>% 
+  filter(age >= 30) %>% 
   group_by(marital) %>% 
   summarise(mean_tv_hours =mean(tvhours, na.rm = T))
+
+tv_hours_table
 
 write_csv(tv_hours_table, here("TV_hours_by_Marital.csv"))
